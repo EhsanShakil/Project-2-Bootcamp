07154b40b6a9eb46de3bb293d42772105e0a5eb3
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-06 11:13:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a bootcamp second project</p>
+        <p>This is a second bootcamp project</p>
         <a
           className="App-link"
           href="https://reactjs.org"
